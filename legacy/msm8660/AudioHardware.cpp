b4cac2bdac943b8365e7a123273f1580fb011ab6
Ivan Grinko <iivanich@gmail.com>
2015-02-10 12:31:53 +0200

    audio/msm8x60: Quick fix for getPresentationPosition build error

--- legacy/msm8660/AudioHardware.cpp
+++ legacy/msm8660/AudioHardware.cpp
@@ -3859,6 +3859,12 @@ status_t AudioHardware::AudioStreamOutMSM8x60::getRenderPosition(uint32_t *dspFr
     return INVALID_OPERATION;
 }
 
+status_t AudioHardware::AudioStreamOutMSM8x60::getPresentationPosition(uint64_t *frames, struct timespec *timestamp)
+{
+    //TODO: enable when supported by driver
+    return INVALID_OPERATION;
+}
+
 #ifdef QCOM_VOIP_ENABLED
 // ----------------------------------------------------------------------------
 // Audio Stream from DirectOutput thread
@@ -4208,6 +4214,13 @@ status_t AudioHardware::AudioStreamOutDirect::getRenderPosition(uint32_t *dspFra
     //TODO: enable when supported by driver
     return INVALID_OPERATION;
 }
+
+status_t AudioHardware::AudioStreamOutDirect::getPresentationPosition(uint64_t *frames, struct timespec *timestamp)
+{
+    //TODO: enable when supported by driver
+    return INVALID_OPERATION;
+}
+
 #endif
 
 // End AudioStreamOutDirect
@@ -5019,6 +5032,12 @@ status_t AudioHardware::AudioSessionOutLPA::getRenderPosition(uint32_t *dspFrame
     return INVALID_OPERATION;
 }
 
+status_t AudioHardware::AudioSessionOutLPA::getPresentationPosition(uint64_t *frames, struct timespec *timestamp)
+{
+    //TODO: enable when supported by driver
+    return INVALID_OPERATION;
+}
+
 status_t AudioHardware::AudioSessionOutLPA::getBufferInfo(buf_info **buf) {
 
     buf_info *tempbuf = (buf_info *)malloc(sizeof(buf_info) + mInputBufferCount*sizeof(int *));
@@ -5810,6 +5829,12 @@ void AudioHardware::AudioSessionOutTunnel::reset()
 }
 
 status_t AudioHardware::AudioSessionOutTunnel::getRenderPosition(uint32_t *dspFrames)
+{
+    //TODO: enable when supported by driver
+    return INVALID_OPERATION;
+}
+
+status_t AudioHardware::AudioSessionOutTunnel::getPresentationPosition(uint64_t *frames, struct timespec *timestamp)
 {
     //TODO: enable when supported by driver
     return INVALID_OPERATION;
--- legacy/msm8660/AudioHardware.h
+++ legacy/msm8660/AudioHardware.h
@@ -277,6 +277,9 @@ private:
                 uint32_t    devices() { return mDevices; }
         virtual status_t    getRenderPosition(uint32_t *dspFrames);
 
+
+        virtual status_t getPresentationPosition(uint64_t *frames, struct timespec *timestamp);
+
     private:
                 AudioHardware* mHardware;
                 int         mFd;
@@ -310,6 +313,8 @@ private:
         virtual String8     getParameters(const String8& keys);
                 uint32_t    devices() { return mDevices; }
         virtual status_t    getRenderPosition(uint32_t *dspFrames);
+
+        virtual status_t getPresentationPosition(uint64_t *frames, struct timespec *timestamp);
 
     private:
                 AudioHardware* mHardware;
@@ -380,6 +385,8 @@ public:
     // return the number of audio frames written by the audio dsp to DAC since
     // the output has exited standby
     virtual status_t    getRenderPosition(uint32_t *dspFrames);
+
+    virtual status_t getPresentationPosition(uint64_t *frames, struct timespec *timestamp);
 
     virtual status_t    getNextWriteTimestamp(int64_t *timestamp);
     virtual status_t    setObserver(void *observer);
