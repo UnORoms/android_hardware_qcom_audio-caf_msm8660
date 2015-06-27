1f675b3efe6195f6e53020031328dec7262e7e90
Shantur Rathore <shantur.rathore@gmail.com>
2015-06-27 23:27:26 +0100

    FM starts but stops

--- legacy/msm8660/AudioHardware.cpp
+++ legacy/msm8660/AudioHardware.cpp
@@ -17,7 +17,7 @@
 
 #include <math.h>
 
-//#define LOG_NDEBUG 0
+#define LOG_NDEBUG 0
 #define LOG_TAG "AudioHardwareMSM8660"
 #include <utils/Log.h>
 #include <utils/String8.h>
@@ -1403,10 +1403,10 @@ status_t AudioHardware::setParameters(const String8& keyValuePairs)
     const char BT_NAME_KEY[] = "bt_headset_name";
     const char BT_NREC_VALUE_ON[] = "on";
 #ifdef QCOM_FM_ENABLED
-    const char FM_NAME_KEY[] = "FMRadioOn";
-    const char FM_VALUE_HANDSET[] = "handset";
-    const char FM_VALUE_SPEAKER[] = "speaker";
-    const char FM_VALUE_HEADSET[] = "headset";
+    const char FM_NAME_KEY[] = "fm_route";
+    const char FM_VALUE_HANDSET[] = "fm_handset";
+    const char FM_VALUE_SPEAKER[] = "fm_speaker";
+    const char FM_VALUE_HEADSET[] = "fm_headset";
     const char FM_VALUE_FALSE[] = "false";
     float fm_volume;
 #endif
@@ -1575,6 +1575,33 @@ status_t AudioHardware::setParameters(const String8& keyValuePairs)
         ALOGV("msm_set_volume(%d) for FM succeeded", fm_volume);
 
         param.remove(key);
+    }
+
+    key = String8(FM_NAME_KEY);
+    if(param.get(key,value) == NO_ERROR)
+    {
+       if(value == FM_VALUE_HANDSET) {
+           fmState  = FM_ON;
+           fmDevice = SND_DEVICE_FM_HANDSET;
+           doRouting(NULL);
+
+       }
+       else if(value == FM_VALUE_SPEAKER) {
+           fmState  = FM_ON;
+           fmDevice = SND_DEVICE_FM_SPEAKER;
+           doRouting(NULL);
+       }
+       if(value == FM_VALUE_HEADSET) {
+           fmState  = FM_ON;
+           fmDevice = SND_DEVICE_FM_HEADSET;
+           doRouting(NULL);
+       }
+       else if(value == FM_VALUE_FALSE) {
+           fmState = FM_OFF;
+           // Need to provide some device id for FM routing to go through
+           fmDevice = SND_DEVICE_FM_HANDSET;
+           doRouting(NULL);
+       }
     }
 #endif /*QCOM_FM_ENABLED*/
 
@@ -2259,6 +2286,41 @@ static status_t do_route_audio_rpc(uint32_t device,
             cur_tx = new_tx_device;
         }
     }
+    else if(fmState == FM_OFF && isStreamOnAndActive(FM_RADIO)) {
+        //disable FM RADIO
+        ALOGV("Disable FM");
+        temp = getNodeByStreamType(FM_RADIO);
+        if(temp == NULL)
+            return -1;
+
+        msm_en_device(DEV_ID(temp->dev_id),0);
+        deleteFromTable(FM_RADIO);
+        fmState = FM_INVALID;
+        fmDevice = INVALID_DEVICE;
+    }
+    else if(fmState == FM_ON && isStreamOnAndInactive(FM_RADIO)) {
+        ALOGV("Enable FM which was in dormant mode");
+        temp = getNodeByStreamType(FM_RADIO);
+        if(temp == NULL)
+            return -1;
+        modifyActiveStateOfStream(FM_RADIO,true);
+        msm_en_device(DEV_ID(temp->dev_id),1);
+    }
+    else if(fmState == FM_ON &&  fmDevice != INVALID_DEVICE) {
+        ALOGV("Going to enable fm radio");
+        if(cur_rx != INVALID_DEVICE)
+            msm_en_device(DEV_ID(cur_rx),0);
+        if(fm_device != INVALID_DEVICE) {
+           if(msm_en_device(DEV_ID(fm_device), 1)) {
+                ALOGE("msm_en_device[%d],1 failed errno = %d",DEV_ID(fm_device),errno);
+                return 0;
+            }
+            addToTable(0,fm_device,INVALID_DEVICE,FM_RADIO,true);
+        }
+        //clear the fm routing info so that future routing to other devices
+        //are not affected
+        fmDevice = INVALID_DEVICE;
+     }
     else {
         ALOGD("updateDeviceInfo() called for default case");
         updateDeviceInfo(new_rx_device,new_tx_device);
@@ -2607,6 +2669,12 @@ status_t AudioHardware::doRouting(AudioStreamInMSM8x60 *input, uint32_t outputDe
         outputDevices = mOutput->devices();
 
     ALOGD("outputDevices = %x", outputDevices);
+
+    // handle fm device routing separately
+    if(fmState != FM_INVALID && fmDevice != INVALID_DEVICE) {
+        ret = doAudioRouteOrMute(fmDevice);
+        return ret;
+    }
 
     if (input != NULL) {
         uint32_t inputDevice = input->devices();
